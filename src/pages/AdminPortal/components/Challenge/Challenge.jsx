5efacce71292ffdeba3f0d29aec6758c5d43b17f
devanshkbhardwaj <87934616+devanshkbhardwaj@users.noreply.github.com>
2025-07-02 11:17:08 +0530

    {feature}:Change in modal

--- src/pages/AdminPortal/components/Challenge/Challenge.jsx
+++ src/pages/AdminPortal/components/Challenge/Challenge.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Header/Header'
 import SearchBar from './components/SearchBar'
 import Prizes from './components/Prizes'
 import AddNewPrizePopup from './components/AddNewPrizePopup'
 
 const Challenge = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleOpenPopup = () => setIsPopupOpen(true);
+  const handleClosePopup = () => setIsPopupOpen(false);
+
   return (
     <div>
       <Header />
 
-      <div className="">
-        <SearchBar />
+      <div className="mb-6">
+        <SearchBar handleOpenPopup={handleOpenPopup} />
+        <button
+          onClick={handleOpenPopup}
+          className="mt-4 text-white font-semibold bg-[#32302F] rounded-[5px] px-4 py-2 hover:bg-[#474747]"
+        >
+          Add Prize
+        </button>
       </div>
 
-      <div className="flex flex-wrap justify-start gap-[50px]">
+      <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-x-[20px] gap-y-[20px] sm:gap-x-[50px] sm:gap-y-[30px] w-full">
         <Prizes />
         <Prizes />
         <Prizes />
       </div>
 
-      {/* Prize Added popup */}
 
+
+      {/* Success Toast */}
       <div className="bg-white p-4 sm:p-[18px] flex items-start justify-between w-[90%] sm:w-full max-w-[390px] border border-[#7CDF79] rounded-[10px] fixed right-4 sm:right-6 bottom-6 sm:top-[calc(100vh-120px)] sm:bottom-auto z-50">
         <div className="pr-2">
           <p className='text-[16px] sm:text-[18px] font-semibold'>Prize Added</p>
@@ -31,11 +43,7 @@ const Challenge = () => {
         </button>
       </div>
 
-
-      {/* Add New Prize popup */}
-      {/* this is modal make this function for close */}
-      <AddNewPrizePopup />
-
+      {isPopupOpen && <AddNewPrizePopup onClose={handleClosePopup} />}
     </div>
   )
 }
--- src/pages/AdminPortal/components/Challenge/components/AddNewPrizePopup.jsx
+++ src/pages/AdminPortal/components/Challenge/components/AddNewPrizePopup.jsx
@@ -1,96 +1,71 @@
 import React from 'react'
 
-const AddNewPrizePopup = () => {
+const AddNewPrizePopup = ({ onClose }) => {
   return (
     <div className='flex items-center justify-center bg-[rgba(0,0,0,0.48)] backdrop-blur-[3px] fixed overflow-auto top-0 left-0 w-full h-full z-[99999999] p-[20px] max-sm:h-auto'>
       <div className="bg-white w-full px-[24px] py-[33px] rounded-[20px] max-w-[448px]">
         <div className="flex items-center justify-between mb-[20px]">
           <h5 className='text-[24px] font-semibold'>Add New Prize</h5>
-          <button className=' cursor-pointer'><img src="src/assets/cross-icon.png" alt="" /></button>
+
+          {/* Close button */}
+          <button onClick={onClose} className='cursor-pointer'>
+            <img src="src/assets/cross-icon.png" alt="Close" />
+          </button>
         </div>
 
         <div className="flex flex-col gap-[14px] max-sm:gap-[10px]">
           <div className="flex items-center flex-col justify-center gap-[10px] border border-dashed border-[#9F9D9E] rounded-[6px] p-[24px]">
-            <img src="src/assets/upload-pic.png" alt="" />
+            <img src="src/assets/upload-pic.png" alt="Upload" />
             <p className=' font-semibold text-[#9F9D9E]'>Upload Logo</p>
           </div>
 
-          <label
-            className="flex flex-col text-[16px] font-semibold gap-[9px]"
-            htmlFor=""
-          >
+          <label className="flex flex-col text-[16px] font-semibold gap-[9px]">
             Prize Name/ Description
-            <input
-              className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]"
-              type="text"
-              placeholder="Enter prize name"
-            />
+            <input className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" type="text" placeholder="Enter prize name" />
           </label>
+
           <div className="flex gap-[16px] max-sm:flex-col">
-            <label
-              className="flex flex-col w-full text-[16px] font-semibold gap-[9px]"
-              htmlFor=""
-            >
+            <label className="flex flex-col w-full text-[16px] font-semibold gap-[9px]">
               Active Date
-              <input
-                className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]"
-                type="date"
-                placeholder="Enter prize name"
-              />
+              <input className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" type="date" />
             </label>
-            <label
-              className="flex flex-col w-full text-[16px] font-semibold gap-[9px]"
-              htmlFor=""
-            >
+            <label className="flex flex-col w-full text-[16px] font-semibold gap-[9px]">
               Active Time
-              <input
-                className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]"
-                type="time"
-                placeholder="Enter prize name"
-              />
+              <input className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" type="time" />
             </label>
           </div>
 
-          <label
-            className="flex flex-col text-[16px] font-semibold gap-[9px]"
-            htmlFor=""
-          >
+          <label className="flex flex-col text-[16px] font-semibold gap-[9px]">
             Challenge
-            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" name="" id="">
+            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]">
               <option value="">Select Challenge</option>
-              <option value="">2</option>
-              <option value="">3</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
             </select>
           </label>
 
-          <label
-            className="flex flex-col text-[16px] font-semibold gap-[9px]"
-            htmlFor=""
-          >
+          <label className="flex flex-col text-[16px] font-semibold gap-[9px]">
             Week
-            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" name="" id="">
+            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]">
               <option value="">Select week</option>
-              <option value="">2</option>
-              <option value="">3</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
             </select>
           </label>
 
-          <label
-            className="flex flex-col text-[16px] font-semibold gap-[9px]"
-            htmlFor=""
-          >
+          <label className="flex flex-col text-[16px] font-semibold gap-[9px]">
             Time Zone
-            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]" name="" id="">
+            <select className="border text-[14px] border-[#DFDFDF] py-[10.5px] px-[12px] rounded-[5px]">
               <option value="">Eastern Time (ET)</option>
-              <option value="">2</option>
-              <option value="">3</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
             </select>
           </label>
 
-          <button className='text-[#fff] font-semibold bg-[#32302F] rounded-[5px] w-full p-[10px] text-center block cursor-pointer hover:bg-[#474747]'>Add Prize</button>
-
+          <button className='text-[#fff] font-semibold bg-[#32302F] rounded-[5px] w-full p-[10px] text-center hover:bg-[#474747]'>
+            Add Prize
+          </button>
         </div>
-
       </div>
     </div>
   )
